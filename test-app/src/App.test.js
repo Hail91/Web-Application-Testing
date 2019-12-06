be0e33151c4308e826f236c0ae9fa47d4072fecd
Aaron <aarongillies88@gmail.com>
2019-12-05 20:32:58 -0500

    Added a few more tests

--- test-app/src/App.test.js
+++ test-app/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 import App from './App';
 
 test('App renders to screen properly', () => {
   render(<App />)
+});
+
+test('Dashboard component renders', () => {
+  render(<Dashboard />)
+});
+
+test('Display component renders', () => {
+  render(<Display />)
 });
 
 test('Foul Button Exists' , () => {
@@ -25,6 +35,8 @@ test('Hit Button exists', () => {
   const { getByText } = render(<App />);
   getByText(/hit/i);
 });
+
+
 
 
 
